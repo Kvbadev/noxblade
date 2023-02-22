f709b69befe66c05899247e84089fb6f87114c95
Jakub Piasecki <jakubpiasecki05@gmail.com>
2023-02-22 20:39:26 +0100

    Finished summary section Added basic form validation changed progressBar to progressForm as it modifies form position for animations Added functionality for edit buttons in summary section

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 import Menu from "./menu.js";
-import ProgressBar from "./progress_bar.js";
+import ProgressForm from "./progress_form.js";
 
 //MENU
 const menu_button = document.querySelector('.hamburger');
@@ -10,35 +10,54 @@ menu_button.addEventListener('click', e => {
 })
 
 //PROGRESS BAR + FORM
-const progressBar = new ProgressBar('.progress-bar .circle', '.progress-bar .line');
+const progressForm = new ProgressForm('.progress-bar .circle', '.progress-bar .line', 'form > span');
 const form_next = document.querySelector('form .next-button');
 const form_prev = document.querySelector('form .prev-button');
-const spans = document.querySelectorAll('form > span');
 const form = document.querySelector('form');
+const position_buttons = document.querySelectorAll('form .btn-position');
 
-console.log(spans);
+const form_values = {};
 
 //TODO: form verification
-const moveForm = () => {
-    for(let [i,el] of spans.entries()) {
-        el.style.left = (i*110)-(progressBar.position*110)+'%';
-    }
-}
 const changePrevButton = isActive => {
     form_prev.disabled = isActive ? false : true; 
     isActive ? form_prev.classList.add('enabled-button') :
     form_prev.classList.remove('enabled-button');
 }
 
-form_prev.onclick = form_next.onclick = e => {
-    e.preventDefault();
-    progressBar.NextPosition();
-    moveForm();
-    changePrevButton(progressBar.position);
+//return index of invalid input or -1 on success
+const validateForm = inputs => {
+    for(const [i, el] of inputs.entries()) {
+        if(!el.value) return i;
+    }
+    return -1;
 }
+
+form_next.onclick = e => {
+    e.preventDefault();
+
+    const inputs = [...form
+        .querySelectorAll(`span:nth-child(${progressForm.position+1}) input`)];
+    const isValid = validateForm(inputs);
+
+    if(isValid === -1) {
+        form_values[progressForm.position] = inputs.map(x => [x.name,x.value]);
+
+        progressForm.NextPosition();
+        changePrevButton(progressForm.position);
+    } else {
+        console.log('Incorrect values: ',isValid);
+    }
+}
+
 form_prev.onclick = e => {
     e.preventDefault();
-    progressBar.PrevPosition();
-    moveForm();
-    changePrevButton(progressBar.position);
+    progressForm.PrevPosition();
+    changePrevButton(progressForm.position);
+}
+
+for(const i of position_buttons) {
+    i.onclick = () => {
+        while(progressForm.position != i.dataset.pos) progressForm.PrevPosition();
+    }
 }
--- scripts/progress_form.js
+++ scripts/progress_form.js
@@ -1,11 +1,13 @@
-export default class ProgressBar {
-    //circles and bar are both class names used for query selection
-    constructor(circles, bar) {
+export default class ProgressForm {
+
+    //circles, bar and spans are all class names used for query selection
+    constructor(circles, bar, spans) {
         this.circles = document.querySelectorAll(circles);
         this.bar = document.querySelector(bar);
         this.position = 0;
-        this.maxPosition = 4;
+        this.maxPosition = 3;
         this.circleWidth = 20;
+        this.spans = document.querySelectorAll(spans);
     }
 
     NextPosition() {
@@ -13,11 +15,18 @@ export default class ProgressBar {
         this.position++;
         this.circles[this.position].classList.add('active');
         this.bar.style.width = this.circles[this.position].offsetLeft+'px';
+        this.MoveForm();
     }
     PrevPosition() {
         if(this.position <= 0) return;
         this.circles[this.position].classList.remove('active');
         this.position--;
         this.bar.style.width = this.circles[this.position].offsetLeft+'px';
+        this.MoveForm();
+    }
+    MoveForm() {
+        for(let [i,el] of this.spans.entries()) {
+            el.style.left = (i*110)-(this.position*110)+'%';
+        }
     }
 }
